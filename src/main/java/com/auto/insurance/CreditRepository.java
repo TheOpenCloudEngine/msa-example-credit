c2db8cab7691969b595bbdf58fd7bc96083247b5
jyjang <jyjang@uengine.org>
2018-10-26 10:04:09 +0900

    no message

--- src/main/java/com/auto/insurance/CreditRepository.java
+++ src/main/java/com/auto/insurance/CreditRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 /**
  * Created by uengine on 2018. 10. 25..
  */
-public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
-    Optional<Customer> findBySsn(String ssn);
+public interface CreditRepository extends PagingAndSortingRepository<Credit, String> {
 }
